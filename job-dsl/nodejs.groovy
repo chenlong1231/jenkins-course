4cae946d0375190f8d17352a88d8ae5708ac2de3
chenlong <2703862+chenlong1231@users.noreply.github.com>
2020-11-03 13:59:02 +0800

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/sharksonline/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/chenlong1231/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
